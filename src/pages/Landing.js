06340fe4a70c1c96bf59539cab894d7ede8af4d9
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2023-07-20 18:02:48 +0900

    [feature]: 공용 검색바 컴포넌트 제작
    재사용 가능한 검색바 컴포넌트 제작, css property로 width, height, fontSize, placeholder, value, onChange 커스텀 가능

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import SearchBar from '../components/SearchBar'
 
 const Landing = () => {
+  const [searchValue, setsearchValue] = useState("")
+  const handleSearch = (e) => {
+    const newValue = e.currentTarget.value;
+    setsearchValue(newValue);
+  }
   return (
     <>
       <Header />
       LandingPage
       <h1>Landing</h1>
+      <SearchBar width="200px" height="" fontSize="20px" placeholder="검색" value={searchValue} onChange={handleSearch} />
+      {searchValue}
       <Footer />
     </>
   )
